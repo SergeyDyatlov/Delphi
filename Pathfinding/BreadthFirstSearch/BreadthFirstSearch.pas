25ac2383be9ae1fb8d1b0aab4ebf0287820596a2
Sergey <application.processmessages@gmail.com>
2024-07-17 14:21:05 +0600

    Универсальный класс для поиска пути в различных структурах данных.

---
+++ Pathfinding/BreadthFirstSearch/BreadthFirstSearch.pas
@@ -0,0 +1,79 @@
+unit BreadthFirstSearch;
+
+interface
+
+uses
+  System.Types, System.Generics.Collections;
+
+type
+  TBreadthFirstSearch<T> = class
+  private
+    FVisited: TDictionary<T, T>;
+    function BuildPath(StartNode, EndNode: T): TArray<T>;
+    function GetNeighbors(Node: T): TArray<T>; virtual; abstract;
+  public
+    function Search(StartNode, EndNode: T): TArray<T>;
+    property Visited: TDictionary<T, T> read FVisited write FVisited;
+  end;
+
+implementation
+
+{ TBreadthFirstSearch }
+
+function TBreadthFirstSearch<T>.BuildPath(StartNode, EndNode: T)
+  : TArray<T>;
+var
+  Path: TList<T>;
+  Current: T;
+begin
+  Path := TList<T>.Create;
+  try
+    Current := EndNode;
+    while Current <> StartNode do
+    begin
+      Path.Insert(0, Current);
+      Current := FVisited[Current];
+    end;
+  finally
+    Path.Free;
+  end;
+end;
+
+function TBreadthFirstSearch<T>.Search(StartNode, EndNode: T)
+  : TArray<T>;
+var
+  Queue: TQueue<T>;
+  Current, Next: T;
+  Neighbors: TArray<T>;
+begin
+  FVisited.Clear;
+  Queue := TQueue<T>.Create;
+  try
+    Queue.Enqueue(StartNode);
+    FVisited.Add(StartNode, Default(T));
+
+    while Queue.Count > 0 do
+    begin
+      Current := Queue.Dequeue;
+      if Current = EndNode then
+        Break;
+
+      Neighbors := GetNeighbors(Current);
+      for Next in Neighbors do
+      begin
+        if not FVisited.ContainsKey(Next) then
+        begin
+          Queue.Enqueue(Next);
+          // Запоминаем откуда пришли к узлу Next
+          FVisited.Add(Next, Current);
+        end;
+      end;
+    end;
+
+    Result := BuildPath(StartNode, EndNode);
+  finally
+    Queue.Free;
+  end;
+end;
+
+end.
