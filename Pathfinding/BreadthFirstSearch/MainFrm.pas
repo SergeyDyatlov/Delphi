d8103d56f7b9906414a6fd6655a5b073799f927f
Sergey <application.processmessages@gmail.com>
2024-07-17 20:57:45 +0600

    Нумерация ячеек

--- Pathfinding/BreadthFirstSearch/MainFrm.pas
+++ Pathfinding/BreadthFirstSearch/MainFrm.pas
@@ -48,15 +48,45 @@ uses CanvasUtils, System.Types;
 procedure TMainForm.DrawGrid(Canvas: TCanvas;
   RowCount, ColCount, CellSize: Integer);
 var
-  X, Y: Integer;
+  Col, Row: Integer;
   GridWidth, GridHeight: Integer;
+  StartPoint, EndPoint: TPoint;
+  CellText: string;
+  TextWidth, TextHeight: Integer;
+  TextX, TextY: Integer;
 begin
   GridWidth := ColCount * CellSize;
   GridHeight := RowCount * CellSize;
-  for X := 0 to ColCount do
-    DrawLine(Canvas, Point(X * CellSize, 0), Point(X * CellSize, GridHeight));
-  for Y := 0 to RowCount do
-    DrawLine(Canvas, Point(0, Y * CellSize), Point(GridWidth, Y * CellSize));
+
+  for Col := 0 to ColCount do
+  begin
+    StartPoint := Point(Col * CellSize, 0);
+    EndPoint := Point(Col * CellSize, GridHeight);
+    DrawLine(Canvas, StartPoint, EndPoint);
+  end;
+
+  for Row := 0 to RowCount do
+  begin
+    StartPoint := Point(0, Row * CellSize);
+    EndPoint := Point(GridWidth, Row * CellSize);
+    DrawLine(Canvas, StartPoint, EndPoint);
+  end;
+
+  Canvas.Brush.Style := bsClear;
+  for Row := 0 to RowCount - 1 do
+  begin
+    for Col := 0 to ColCount - 1 do
+    begin
+      CellText := Format('%d, %d', [Col, Row]);
+      TextWidth := Canvas.TextWidth(CellText);
+      TextHeight := Canvas.TextHeight(CellText);
+
+      TextX := (Col * CellSize) + (CellSize div 2) - (TextWidth div 2);
+      TextY := (Row * CellSize) + (CellSize div 2) - (TextHeight div 2);
+
+      Canvas.TextOut(TextX, TextY, CellText);
+    end;
+  end;
 end;
 
 procedure TMainForm.FormCreate(Sender: TObject);
@@ -74,9 +104,6 @@ begin
   FGrid.Walls.Add(Point(5, 7));
   FGrid.Walls.Add(Point(5, 5));
   FGrid.Walls.Add(Point(5, 6));
-  FGrid.Walls.Add(Point(1, 6));
-  FGrid.Walls.Add(Point(2, 6));
-  FGrid.Walls.Add(Point(3, 6));
   FGrid.Walls.Add(Point(5, 10));
   FGrid.Walls.Add(Point(5, 11));
   FGrid.Walls.Add(Point(5, 12));
@@ -120,12 +147,17 @@ begin
   FGrid.Walls.Add(Point(10, 3));
   FGrid.Walls.Add(Point(9, 3));
   FGrid.Walls.Add(Point(11, 3));
+
   FGrid.Walls.Add(Point(2, 5));
   FGrid.Walls.Add(Point(2, 4));
   FGrid.Walls.Add(Point(2, 3));
   FGrid.Walls.Add(Point(2, 2));
-  FGrid.Walls.Add(Point(2, 0));
   FGrid.Walls.Add(Point(2, 1));
+  FGrid.Walls.Add(Point(2, 0));
+  FGrid.Walls.Add(Point(1, 6));
+  FGrid.Walls.Add(Point(2, 6));
+  FGrid.Walls.Add(Point(3, 6));
+
   FGrid.Walls.Add(Point(0, 11));
   FGrid.Walls.Add(Point(1, 11));
   FGrid.Walls.Add(Point(2, 11));
@@ -144,6 +176,14 @@ begin
   FGrid.Walls.Add(Point(5, 3));
   FGrid.Walls.Add(Point(6, 3));
   FGrid.Walls.Add(Point(5, 4));
+
+  FGrid.Walls.Add(Point(24, 0));
+  FGrid.Walls.Add(Point(24, 1));
+  FGrid.Walls.Add(Point(24, 2));
+  FGrid.Walls.Add(Point(24, 3));
+  FGrid.Walls.Add(Point(25, 3));
+  FGrid.Walls.Add(Point(26, 3));
+  FGrid.Walls.Add(Point(27, 3));
 
   FPathFinder := TPathFinder.Create(FGrid);
   FSearchPath := FPathFinder.Search(StartPoint, EndPoint);
